504d5656ba83f66ef97411cadc3992c442cb162d
Li Mingyu <e1484123@u.nus.edu>
2025-06-29 16:05:37 +0800

    backend for deploy

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,15 @@ const cors = require("cors");
 const db = require("./db");
 const initDatabase = require("./db/init");
 
+const authRoutes = require("./routes/auth");
+const recommendRoutes = require("./routes/recommend");
+const dailyRoutes = require("./routes/daily");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-
-const authRoutes = require("./routes/auth");
-const recommendRoutes = require("./routes/recommend");
-const dailyRoutes = require("./routes/daily");
-const spotifyProxy = require('./routes/spotifyProxy');
-app.use('/proxy', spotifyProxy); 
 
 app.use("/auth", authRoutes);
 app.use("/recommend", recommendRoutes);
